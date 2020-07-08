0a979bf969191d0bae156329ea4434a3781ff05f
Kai Jones <kaijones@hawaii.edu>
2020-07-08 00:15:33 -1000

    Changes are up to date.

--- gnss/functions.ino
+++ gnss/functions.ino
@@ -10,43 +10,58 @@ void setupOLED(MicroOLED &oled){
 }
 
 void setupGNSS(SFE_UBLOX_GPS &gnss){
-  gnss.begin(Wire);   // Setup GNSS for Serial communication at default value
 
-  //Check to see if GNSS is connected
+  //-------------------------------------------------------------
+  // Connect to the Ublox module using Wire port at default value
+  // and check to see if gnss is connected. 
+  //-------------------------------------------------------------  
+  gnss.begin(Wire);                                      // Setup GNSS for Serial communication at default value
   if(gnss.isConnected() == false){
-    printOLED("Initial GNSS setup failed. Freezing.");   // Print error message to oled
+    //printOLED("Initial GNSS setup failed. Freezing.");   // Print error message to oled
     while(1);                                            // Freeze program (infinite loop)
   }
   else if(gnss.isConnected() == true){
-    printOLED("Setting up GNSS module...");              // Print message 
+    //printOLED("Setting up GNSS module...");              // Print message
+    delay(2000);                                         // To allow LCD to have message for a brief moment 
   }
 
-  //Check to see if Survey is in Progress before initiating one
-  boolean surveyStatus;
-  surveyStatus = checkSurveyStatus(gnss);
-  if(surveyStatus == false){
-    printOLED("Failed to get Survey In status. Freezing.");  // Print error message to oled
+  //-----------------------------------------------------------
+  //Check to see if Survey is in progress before initiating one
+  //-----------------------------------------------------------
+  response = gnss.getSurveyStatus(2000); // Query rtk module for SVIN status for time [ms] 
+  
+  if(response == false){
+    //printOLED("Failed to get Survey In status. Freezing.");  // Print error message to oled
     while (1);                                               // Freeze program (infinite loop)
   }
-  else if(surveyStatus == true){
-    printOLED("Survey is already in progress.");
+  
+  if(gnss.svin.active == true){
+    //printOLED("Survey is already in progress.");
+    delay(2000);
   }
   else{
-    boolean response = gnss.enableSurveyMode(300, 2.000); // Enable Survey In. Minimum time is 300 seconds and minimum accuracy of 2.000 meters accuracy achieved.  
+    response = gnss.enableSurveyMode(minSurveyTime, minSurveyAcc); // Enable Survey In. Minimum time is 300 seconds and minimum accuracy of 1.000 meters accuracy achieved.  
     if(response == false){
-      printOLED("Survey failed. Freezing.");
+      //printOLED("Survey failed to start. Freezing.");
       while(1);
     }
-    printOLED("Survey has begun."); 
+    oled.clear(PAGE); 
+    oled.print(F("Survey has begun. Minimum time: "));
+    oled.print(minSurveyTime);
+    oled.print(F(". \nMinimum Acc: "));
+    oled.print(minSurveyAcc);
+    oled.display();
+    delay(2000);
   }
   gnss.setI2COutput(COM_TYPE_UBX);
   gnss.saveConfiguration();
 }
 
 void printOLED(String msg){
-  oled.clear(PAGE); // Clear the buffer
-  oled.print(msg);  // Save message to buffer
-  oled.display();   // Display buffer
+  oled.clear(PAGE);     // Clear the buffer
+  oled.setCursor(0,0);  // Set cursor in top left corner
+  oled.print(msg);      // Save message to buffer
+  oled.display();       // Display buffer
 }
 
 boolean checkSurveyStatus(SFE_UBLOX_GPS &gnss){
@@ -58,4 +73,23 @@ boolean checkSurveyStatus(SFE_UBLOX_GPS &gnss){
   if(gnss.svin.active == true){
     return true;
   }
+}
+
+void enableRTCM(SFE_UBLOX_GPS &gnss){
+  response = true;
+  response &= gnss.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); //Enable message 1005 to output through I2C port, message every second
+  response &= gnss.enableRTCMmessage(UBX_RTCM_1077, COM_PORT_I2C, 1);
+  response &= gnss.enableRTCMmessage(UBX_RTCM_1087, COM_PORT_I2C, 1);
+  response &= gnss.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
+
+  if (response == true){
+    //printOLED("RTCM messages enabled");
+    delay(1000);
+  }
+  else{
+    //printOLED("RTCM enable failed. Freezing.");
+    while (1); //Freeze
+  }
+  
+  //printOLED("Base survey complete! RTCM now broadcasting.");
 }
--- gnss/gnss.ino
+++ gnss/gnss.ino
@@ -9,6 +9,14 @@
 #define DC_JUMPER 1
 
 //************************************************
+//               SYSTEM VARIABLES
+//************************************************
+boolean response;            // variable used for gnss setup
+float minSurveyTime = 300;   // minimum amount of time [sec] for Survey-In to do
+float minSurveyAcc = 1.000;  // minimum positional accuracy [m] Survey-In must achieve
+long lastTime = 0;           // simple local timer. Limits amount of I2C traffic to Ublox module.
+
+//************************************************
 //              OBJECT INSTANCES
 //************************************************
 MicroOLED oled(PIN_RESET, DC_JUMPER);  // create oled object
@@ -21,8 +29,47 @@ void setup() {
   Wire.begin();           // initiate I2C comms bus
   setupOLED(oled);        // setup the oled
   setupGNSS(baseStation); // setup basestation GNSS
+  
+  //-----------------------------
+  // Wait for Survey-In to finish
+  //-----------------------------
+  while(baseStation.svin.valid == false){
+    response = baseStation.getSurveyStatus(2000); //Query module for SVIN status with 2000 ms timeout.
+    if (response == true){
+      oled.clear(PAGE);
+      oled.setCursor(0,0);
+      oled.print(F("Time: "));
+      oled.print((String)baseStation.svin.observationTime);
+      oled.print(F("\nAccuracy: "));
+      oled.print((String)baseStation.svin.meanAccuracy);
+      oled.display();
+    }
+    delay(1000);
+  }
+
+  printOLED("Survey finished!\nEnabling RTCM msgs");
+  delay(1000);
+  
+  //---------------------
+  // Enable RTCM Messages
+  //---------------------
+  enableRTCM(baseStation);
+
 }
 
 void loop() {
-
+  baseStation.checkUblox(); // See if new data is available. Process bytes as they come in. 
+  /*if ((millis() - lastTime > 1000) && (flag == true)){
+    lastTime = millis(); // Update timer
+    
+    long latitude = baseStation.getLatitude();
+    long longitude = baseStation.getLongitude();
+    oled.clear(PAGE);
+    oled.setCursor(0,0); 
+    oled.print("Lat:");
+    oled.print((String)latitude);
+    oled.print("\nLon:");
+    oled.print((String)longitude);
+    oled.display();
+  }*/
 }
