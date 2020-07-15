eec0b3a8ecaf02dcd7523aa2a71766dee01fc22d
Kai Jones <kaijones@hawaii.edu>
2020-07-15 13:23:22 -1000

    New files. Trying to do some memory management. Might have to switch to the MEGA.

--- gnss/functions.ino
+++ gnss/functions.ino
@@ -1,28 +1,22 @@
-void setupOLED(MicroOLED &oled){
-  delay(100);            // Wait 100 ms
-  oled.begin();          // Initialize the OLED
-  oled.clear(ALL);       // Clear the display's internal memory
-  oled.display();        // Display what's in buffer (should just be the SparkFun splashscreen)
-  delay(1000);           // Delay 1000 ms
-  oled.clear(PAGE);      // Clear the buffer
-  oled.setFontType(0);
-  oled.setCursor(0,0);
-}
-
 void setupGNSS(SFE_UBLOX_GPS &gnss){
-
+  //----------------------------
+  // Initialize local variables
+  //----------------------------
+  boolean response;            // variable used for gnss setup
+  int minSurveyTime = 300;     // minimum amount of time [sec] for Survey-In to do
+  float minSurveyAcc = 1.000;  // minimum positional accuracy [m] Survey-In must achieve
   //-------------------------------------------------------------
   // Connect to the Ublox module using Wire port at default value
   // and check to see if gnss is connected. 
   //-------------------------------------------------------------  
-  gnss.begin(Wire);                                      // Setup GNSS for Serial communication at default value
+  gnss.begin(Wire);                                            // Setup GNSS for Serial communication at default value
   if(gnss.isConnected() == false){
-    //printOLED("Initial GNSS setup failed. Freezing.");   // Print error message to oled
-    while(1);                                            // Freeze program (infinite loop)
+    Serial.print(F("Initial GNSS setup failed. Freezing.\n")); // Print error message to oled
+    
+    while(1);                                                  // Freeze program (infinite loop)
   }
   else if(gnss.isConnected() == true){
-    //printOLED("Setting up GNSS module...");              // Print message
-    delay(2000);                                         // To allow LCD to have message for a brief moment 
+    Serial.print(F("Setting up GNSS module...\n")); // Print message
   }
 
   //-----------------------------------------------------------
@@ -31,65 +25,55 @@ void setupGNSS(SFE_UBLOX_GPS &gnss){
   response = gnss.getSurveyStatus(2000); // Query rtk module for SVIN status for time [ms] 
   
   if(response == false){
-    //printOLED("Failed to get Survey In status. Freezing.");  // Print error message to oled
-    while (1);                                               // Freeze program (infinite loop)
+    Serial.print(F("Failed to get Survey In status. Freezing.\n"));
+    while (1); // Freeze program (infinite loop)
   }
-  
   if(gnss.svin.active == true){
-    //printOLED("Survey is already in progress.");
-    delay(2000);
+    Serial.print(F("Survey is already in progress.\n"));
   }
   else{
     response = gnss.enableSurveyMode(minSurveyTime, minSurveyAcc); // Enable Survey In. Minimum time is 300 seconds and minimum accuracy of 1.000 meters accuracy achieved.  
     if(response == false){
-      //printOLED("Survey failed to start. Freezing.");
+      Serial.print(F("Survey failed to start. Freezing.\n"));
       while(1);
     }
-    oled.clear(PAGE); 
-    oled.print(F("Survey has begun. Minimum time: "));
-    oled.print(minSurveyTime);
-    oled.print(F(". \nMinimum Acc: "));
-    oled.print(minSurveyAcc);
-    oled.display();
-    delay(2000);
+    Serial.print(F("Survey has begun. Minimum time: "));
+    Serial.print(minSurveyTime);
+    Serial.print(F(".\tMinimum Accuracy: "));
+    Serial.println(minSurveyAcc);
   }
   gnss.setI2COutput(COM_TYPE_UBX);
   gnss.saveConfiguration();
 }
 
-void printOLED(String msg){
-  oled.clear(PAGE);     // Clear the buffer
-  oled.setCursor(0,0);  // Set cursor in top left corner
-  oled.print(msg);      // Save message to buffer
-  oled.display();       // Display buffer
+void waitForSurvey(){
+  boolean response;
+  while(baseStation.svin.valid == false){
+    response = baseStation.getSurveyStatus(2000); // query module for SVIN status with 2000 ms timeout.
+    if(response == true){
+      Serial.print(F("Time: "));
+      Serial.print((String)baseStation.svin.observationTime);
+      Serial.print(F("\tAccuracy: "));
+      Serial.println((String)baseStation.svin.meanAccuracy);
+    }
+  Serial.println(F("Survey finished. Enabling RTCM messages."));
+  }
 }
 
-boolean checkSurveyStatus(SFE_UBLOX_GPS &gnss){
+void enableRTCM(){
   boolean response; 
-  response = gnss.getSurveyStatus(2000); // Query rtk module for SVIN status for time [ms] 
-  if(response == false){
-    return false;
-  }
-  if(gnss.svin.active == true){
-    return true;
-  }
-}
-
-void enableRTCM(SFE_UBLOX_GPS &gnss){
   response = true;
-  response &= gnss.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); //Enable message 1005 to output through I2C port, message every second
-  response &= gnss.enableRTCMmessage(UBX_RTCM_1077, COM_PORT_I2C, 1);
-  response &= gnss.enableRTCMmessage(UBX_RTCM_1087, COM_PORT_I2C, 1);
-  response &= gnss.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
-
+  response &= baseStation.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); //Enable message 1005 to output through I2C port, message every second
+  response &= baseStation.enableRTCMmessage(UBX_RTCM_1077, COM_PORT_I2C, 1);
+  response &= baseStation.enableRTCMmessage(UBX_RTCM_1087, COM_PORT_I2C, 1);
+  response &= baseStation.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
+  
   if (response == true){
-    //printOLED("RTCM messages enabled");
-    delay(1000);
+    Serial.print(F("\nRTCM messages enabled.\n"));
   }
   else{
-    //printOLED("RTCM enable failed. Freezing.");
-    while (1); //Freeze
+    Serial.print(F("\nRTCM messages failed to enable. Freezing."));
+    while (1); // freeze
   }
-  
-  //printOLED("Base survey complete! RTCM now broadcasting.");
+  Serial.print(F("Base survey complete! RTCM now broadcasting.\n"));
 }
--- gnss/gnss.ino
+++ gnss/gnss.ino
@@ -1,75 +1,53 @@
-#include <SFE_MicroOLED.h>
+#include "ros.h"
+#include<std_msgs/Int64.h>
 #include <SparkFun_Ublox_Arduino_Library.h>
 #include <Wire.h>
 
 //************************************************
-//               PIN DEFINITIONS
-//************************************************
-#define PIN_RESET 9
-#define DC_JUMPER 1
-
-//************************************************
 //               SYSTEM VARIABLES
 //************************************************
-boolean response;            // variable used for gnss setup
-float minSurveyTime = 300;   // minimum amount of time [sec] for Survey-In to do
-float minSurveyAcc = 1.000;  // minimum positional accuracy [m] Survey-In must achieve
-long lastTime = 0;           // simple local timer. Limits amount of I2C traffic to Ublox module.
+long lastTime = 0;  // simple local timer. Limits amount of I2C traffic to Ublox module.
+long longitude = 0; // variable to store longitude
+long latitude = 0;  // variable to store latitude
 
 //************************************************
 //              OBJECT INSTANCES
 //************************************************
-MicroOLED oled(PIN_RESET, DC_JUMPER);  // create oled object
-SFE_UBLOX_GPS baseStation;             // create rtk gnss object
+SFE_UBLOX_GPS baseStation;                    // create rtk gnss object
+ros::NodeHandle nh;                           // ROS node handler. Allows us to create publishers and subscribers.
+std_msgs::Int64 intLat;                       // ROS message type to store latitude
+std_msgs::Int64 intLon;                       // ROS message type to store longitude
+ros::Publisher latPub("latitude", &intLat);   // ROS publisher for latitude
+ros::Publisher lonPub("longitude", &intLon);  // ROS publisher for longitude
 
 //************************************************
 //                   SETUP
 //************************************************
 void setup() {
   Wire.begin();           // initiate I2C comms bus
-  setupOLED(oled);        // setup the oled
-  setupGNSS(baseStation); // setup basestation GNSS
-  
-  //-----------------------------
-  // Wait for Survey-In to finish
-  //-----------------------------
-  while(baseStation.svin.valid == false){
-    response = baseStation.getSurveyStatus(2000); //Query module for SVIN status with 2000 ms timeout.
-    if (response == true){
-      oled.clear(PAGE);
-      oled.setCursor(0,0);
-      oled.print(F("Time: "));
-      oled.print((String)baseStation.svin.observationTime);
-      oled.print(F("\nAccuracy: "));
-      oled.print((String)baseStation.svin.meanAccuracy);
-      oled.display();
-    }
-    delay(1000);
-  }
-
-  printOLED("Survey finished!\nEnabling RTCM msgs");
-  delay(1000);
-  
-  //---------------------
-  // Enable RTCM Messages
-  //---------------------
-  enableRTCM(baseStation);
-
+  nh.initNode();          // initialize ROS node handle
+  nh.advertise(latPub);   // advertise latitude publisher
+  nh.advertise(lonPub);   // advertise longitude publisher
+  setupGNSS(baseStation); // setup GNSS 
+  waitForSurvey();        // wait for Survey-In to finish
+  enableRTCM();           // enable RTCM messages
 }
 
 void loop() {
-  baseStation.checkUblox(); // See if new data is available. Process bytes as they come in. 
-  /*if ((millis() - lastTime > 1000) && (flag == true)){
+  baseStation.checkUblox();                 // see if new data is available. Process bytes as they come in. 
+  if (millis() - lastTime > 1000){
     lastTime = millis(); // Update timer
-    
-    long latitude = baseStation.getLatitude();
-    long longitude = baseStation.getLongitude();
-    oled.clear(PAGE);
-    oled.setCursor(0,0); 
-    oled.print("Lat:");
-    oled.print((String)latitude);
-    oled.print("\nLon:");
-    oled.print((String)longitude);
-    oled.display();
-  }*/
+    latitude = baseStation.getLatitude();
+    longitude = baseStation.getLongitude();
+    Serial.print(F("Latitude: "));
+    Serial.print(latitude);
+    Serial.print(F("\tLongitude: "));
+    Serial.println(longitude);
+
+    intLat.data = latitude;
+    intLon.data = longitude;
+    latPub.publish( &intLat );
+    lonPub.publish( &intLon );
+    nh.spinOnce();
+  }
 }
